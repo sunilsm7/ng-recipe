af311c80d04f7f5abc5e38b144785fdece4802de
Sunil <sunil9766@gmail.com>
2019-10-20 12:38:43 +0530

    add: environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseAPIKey: 'AIzaSyCLsc7fj3YLglmTPp1rYiLVjvztQHTN2Os',
+  authUrl: 'https://www.googleapis.com/identitytoolkit/v3/relyingparty',
+  firebaseUrl: 'https://angular-9c541.firebaseio.com'
 };
